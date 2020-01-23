c2bb32cf71ec54aaf00a9dc1673c94c799aff382
overmind121 <lukekim1029@gmail.com>
2020-01-22 19:21:42 -0800

    Straigt up making motors go with no time delays

--- motors/motors.ino
+++ motors/motors.ino
@@ -3,6 +3,7 @@
 //Backward is char character: 98
 //Left is char character: 108
 //right is char character: 114
+//stop is char characterL 83
 AF_DCMotor motorL(3, MOTOR34_64KHZ);
 AF_DCMotor motorR(4, MOTOR34_64KHZ);
 
@@ -13,12 +14,27 @@ void setup(){
 }
 
 void loop(){
-  if(Serial.read() == 102){
+  if(Serial.read() == 102){ //forward
     motorL.run(FORWARD);
     motorR.run(FORWARD);
-    delay(5000);
+
+  }
+  if(Serial.read() == 98){ //backward
+    motorL.run(BACKWARD);
+    motorR.run(BACKWARD);
+
+  }
+  if(Serial.read() == 108){ //Left
+    motorL.run(FORWARD);
+    motorR.run(RELEASE);
+  }
+  if(Serial.read() == 114){ //right
+    motorL.run(RELEASE);
+    motorR.run(FORWARD);
+  } 
+  if(Serial.read() == 83){
     motorL.run(RELEASE);
     motorR.run(RELEASE);
-    delay(1000);
-  } 
-}
+  }
+  
+ }
